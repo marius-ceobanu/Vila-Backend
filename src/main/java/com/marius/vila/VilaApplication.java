7a28810b65907fbef9068d648583ff57664906e3
Marius Ceobanu <ceobanu.marius@gmail.com>
2021-04-13 16:57:04 +0300

    first commit*

---
+++ src/main/java/com/marius/vila/VilaApplication.java
@@ -0,0 +1,13 @@
+package com.marius.vila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VilaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marius/vila/VilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marius.vila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
