e3bb82a9d46351e5be83f060b22c068d6c061422
Marius Ceobanu <ceobanu.marius@gmail.com>
2021-04-14 13:56:52 +0300

    Implement Amenity Controller VILA-43

---
+++ src/main/java/com/marius/vila/amenity/controller/AmenityController.java
@@ -0,0 +1,27 @@
+package com.marius.vila.amenity.controller;
+
+import com.marius.vila.amenity.model.Amenity;
+import com.marius.vila.amenity.service.AmenityService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/vila/v1/amenities")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
+public class AmenityController {
+
+    private final AmenityService amenityService;
+
+    @Transactional
+    @GetMapping
+    public List<Amenity> getAllAmenities() {
+        return amenityService.getAllAmenities();
+    }
+}
