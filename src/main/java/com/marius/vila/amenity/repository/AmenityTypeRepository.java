efd2fef87239eb20ac9d7e23d14e34bb3672c5d6
Marius Ceobanu <ceobanu.marius@gmail.com>
2021-04-14 12:55:18 +0300

    Implement AmenityRepository VILA-41

---
+++ src/main/java/com/marius/vila/amenity/repository/AmenityTypeRepository.java
@@ -0,0 +1,8 @@
+package com.marius.vila.amenity.repository;
+
+import com.marius.vila.amenity.model.AmenityType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AmenityTypeRepository extends JpaRepository<AmenityType, Long> {}
--- src/main/java/com/marius/vila/room/model/Room.java
+++ src/main/java/com/marius/vila/room/model/Room.java
@@ -1,10 +1,14 @@
 package com.marius.vila.room.model;
 
+import com.marius.vila.amenity.model.Amenity;
+import com.marius.vila.reservation.model.Reservation;
 import lombok.*;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,4 +31,24 @@ public class Room {
 
     @NotNull
     private Float price;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "amenity_id", referencedColumnName = "id")
+    private List<Amenity> amenities = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id", referencedColumnName = "id")
+    private List<Reservation> reservations = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_type_id", referencedColumnName = "id")
+    private RoomType roomType;
+
+    public void addAmenity(Amenity amenity) {
+        amenities.add(amenity);
+    }
+
+    public void addReservation(Reservation reservation) {
+        reservations.add(reservation);
+    }
 }
